4a44b8cfb4c0ebfd1bc3d111309ce6a7aedc065e
jonathanciapetti <jonathanciapetti@gmx.com>
2024-06-20 17:47:30 +0200

    Fixed modules names

--- picklevw.py
+++ picklevw.py
@@ -3,8 +3,8 @@ foobar
 """
 from __future__ import annotations
 
-from src.custom_widgets import set_options
-from src.custom_window import CustomWindow
+from src.widgets import set_options
+from src.window import CustomWindow
 from src.mediator import Mediator
 from src.logic import output_queue, terminate_all_processes
 
@@ -30,10 +30,3 @@ cw.update_text_widget_from_queue(cw.widgets["example"], output_queue)
 cw.protocol('WM_DELETE_WINDOW', exit_function)
 
 cw.mainloop()
-
-# Setup the Tree
-# tree = ttk.Treeview(example_frame, columns='Values')
-# json_tree(tree, '',  ast.literal_eval(med._pickle_str) or dict())
-# tree.grid()
-# window.state('normal')
-# window.grid_propagate(False)
