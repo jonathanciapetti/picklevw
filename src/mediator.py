43cd931ca2079bea230711b310ab9175f7748cba
jonathanciapetti <jonathanciapetti@gmx.com>
2024-06-25 20:22:45 +0200

    Docs

--- src/mediator.py
+++ src/mediator.py
@@ -1,5 +1,6 @@
 """
-foobar
+This module defines the implementation of the Mediator pattern (from the Gang of 4 book).
+It is just an attempt, so it must be assumed as flawed.
 """
 
 from src.widgets import LoadButton, ThemeButton
@@ -8,21 +9,17 @@ from src.logic import start_process
 
 
 class Mediator:
-    """ xyzt
-    """
+    """Mediator class to manage interactions between buttons and the main window."""
 
     def __init__(self, elements: list, window: CustomWindow):
-        """ aaa
-
-        :param elements:
-        :param window:
-        """
-        self.elements = elements
-        self.button_receivers = []
+        self.elements = elements  # List of button elements
+        self.button_receivers = []  # List to hold button receivers
         self.window_ = window
 
+        # Bind key events in the window to a control event handler
         self.window_.bind("<Key>", lambda e: self.window_.ctrl_events(e))
 
+        # Bind button press events to their respective handlers
         for elem in self.elements:
             if isinstance(elem, LoadButton):
                 elem.bind('<ButtonPress>', (lambda _: start_process()))
@@ -30,6 +27,8 @@ class Mediator:
                 elem.bind('<ButtonPress>', (lambda _: self.switch_theme()))
 
     def switch_theme(self):
+        """Switches the theme of the text widget between light and dark modes."""
+
         current_bg = self.window_.widgets["example"].text["background"]
         if current_bg == "white":
             self.window_.widgets["example"].text.configure(
