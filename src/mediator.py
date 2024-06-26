b0dc03b6c41f69457476bffb4322a1d563919a90
jonathanciapetti <jonathanciapetti@gmx.com>
2024-06-26 13:37:16 +0200

    Fixed binding

--- src/mediator.py
+++ src/mediator.py
@@ -15,26 +15,30 @@ class Mediator:
 
         # Bind button press events to their respective handlers
         for elem in self.elements:
+
+            window = None
+            if isinstance(elem, PicklevwTkWindow):
+                window = elem
+                elem.bind('<Key>', (lambda e: window.ctrl_events(e)))
+
             if isinstance(elem, PicklevwTkLoadButton):
                 elem.bind('<ButtonPress>', (lambda _: start_process()))
             elif isinstance(elem, PicklevwTkThemeButton):
-                elem.bind('<ButtonPress>', (lambda _: self.switch_theme()))
-            elif isinstance(elem, PicklevwTkWindow):
-                elem.bind('<Key>', (lambda e: self.window_.ctrl_events(e)))
+                elem.bind('<ButtonPress>', (lambda _: self.switch_theme(window)))
 
-    def switch_theme(self) -> None:
+    def switch_theme(self, window) -> None:
         """ Switches the theme of the text widget between light and dark mode. """
 
-        current_bg = self.window_.widgets["picklevw_tk_frame"].text["background"]
+        current_bg = window.widgets["picklevw_tk_frame"].text["background"]
         if current_bg == "white":
-            self.window_.widgets["picklevw_tk_frame"].text.configure(
+            window.widgets["picklevw_tk_frame"].text.configure(
                 background="black",
                 foreground="white",
             )
-            self.window_.widgets["btn_theme"]["text"] = "Switch to light mode"
+            window.widgets["btn_theme"]["text"] = "Switch to light mode"
         else:
-            self.window_.widgets["picklevw_tk_frame"].text.configure(
+            window.widgets["picklevw_tk_frame"].text.configure(
                 background="white",
                 foreground="black",
             )
-            self.window_.widgets["btn_theme"]["text"] = "Switch to dark mode"
+            window.widgets["btn_theme"]["text"] = "Switch to dark mode"
