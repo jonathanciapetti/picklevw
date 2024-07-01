9658241e16a0b262bbc5f1e6a3cf4b97a37df3a3
jonathanciapetti <jonathanciapetti@gmx.com>
2024-07-01 12:11:46 +0200

    Uncommented everything

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,24 +1,24 @@
-# import pytest
-# from src.widgets import PicklevwTkLoadButton, PicklevwTkThemeButton, PicklevwTkFrame
-# from src.factory import WidgetFactory
-#
-#
-# def test_create_load_button():
-#     load_button = WidgetFactory.create_widget('load_button')
-#     assert isinstance(load_button, PicklevwTkLoadButton)
-#
-#
-# def test_create_theme_button():
-#     theme_button = WidgetFactory.create_widget('theme_button')
-#     assert isinstance(theme_button, PicklevwTkThemeButton)
-#
-#
-# def test_create_frame():
-#     frame = WidgetFactory.create_widget('frame', name='picklevw')
-#     assert isinstance(frame, PicklevwTkFrame)
-#
-#
-# def test_create_unknown_widget():
-#     with pytest.raises(ValueError) as excinfo:
-#         WidgetFactory.create_widget('unknown')
-#     assert str(excinfo.value) == "Unknown widget type: unknown"
+import pytest
+from src.widgets import PicklevwTkLoadButton, PicklevwTkThemeButton, PicklevwTkFrame
+from src.factory import WidgetFactory
+
+
+def test_create_load_button():
+    load_button = WidgetFactory.create_widget('load_button')
+    assert isinstance(load_button, PicklevwTkLoadButton)
+
+
+def test_create_theme_button():
+    theme_button = WidgetFactory.create_widget('theme_button')
+    assert isinstance(theme_button, PicklevwTkThemeButton)
+
+
+def test_create_frame():
+    frame = WidgetFactory.create_widget('frame', name='picklevw')
+    assert isinstance(frame, PicklevwTkFrame)
+
+
+def test_create_unknown_widget():
+    with pytest.raises(ValueError) as excinfo:
+        WidgetFactory.create_widget('unknown')
+    assert str(excinfo.value) == "Unknown widget type: unknown"
