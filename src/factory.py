d2846efc22867a52e47086933255a7ed98b875f8
jonathanciapetti <jonathanciapetti@gmx.com>
2024-07-01 12:11:21 +0200

    Docstrings

--- src/factory.py
+++ src/factory.py
@@ -1,13 +1,30 @@
-from typing import TypeAlias
+"""
+Module for creating and managing custom widgets in the Picklevw application.
+"""
 
+from typing import TypeAlias
 from src.widgets import PicklevwTkLoadButton, PicklevwTkThemeButton, PicklevwTkFrame
 
 WidgetType: TypeAlias = PicklevwTkLoadButton | PicklevwTkThemeButton | PicklevwTkFrame
 
 
 class WidgetFactory:
+    """ Factory class for creating widgets used in the Picklevw application.
+    """
+
     @staticmethod
     def create_widget(widget_type: str, *args, **kwargs) -> WidgetType:
+        """ Create and return a widget of the specified type.
+
+        :param widget_type: The type of widget to create. Must be one of
+                            'load_button', 'theme_button', or 'frame'.
+        :type widget_type: str
+        :param args: widget initializer args.
+        :param kwargs: widget initializer kwargs.
+        :return: An instance of the specified widget type.
+        :rtype: WidgetType
+        :raises ValueError: If the specified widget type is not recognized.
+        """
         if widget_type == "load_button":
             return PicklevwTkLoadButton(*args, **kwargs)
         elif widget_type == "theme_button":
